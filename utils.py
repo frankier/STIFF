4b7f19d811abdd646096102a9712b13b8592bfe9
Frankie Robertson <frankie@robertson.name>
2018-05-31 08:41:30 +0300

    Fix path to t2s_char.json

--- utils.py
+++ utils.py
@@ -9,6 +9,6 @@ def get_opencc():
     global _opencc
     if _opencc is None:
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        opencc_config = os.path.join(dir_path, 't2s_char.json')
+        opencc_config = os.path.join(dir_path, '../t2s_char.json')
         _opencc = opencc.OpenCC(opencc_config)
     return _opencc

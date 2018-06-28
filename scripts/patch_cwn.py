009db6ca1782955c660bce062f5f136185594a56
Frankie Robertson <frankie@robertson.name>
2018-06-28 10:25:27 +0200

    Fix patch_cwn

--- scripts/patch_cwn.py
+++ scripts/patch_cwn.py
@@ -7,8 +7,8 @@ from plumbum.cmd import patch
 fn, headers = urlretrieve("http://compling.hss.ntu.edu.sg/omw/wns/cmn.zip")
 assert headers["Content-Type"] == "application/zip"
 zip = zipfile.ZipFile(fn, "r")
-old = zip.open("cow/wn-data-cmn-fixed.tab")
-with open("wn-data-cmn.tab", "wb") as f_out:
+old = zip.open("cow/wn-data-cmn.tab")
+with open("wn-data-cmn-fixed.tab", "wb") as f_out:
     shutil.copyfileobj(old, f_out)
 
 patch("wn-data-cmn-fixed.tab", "wn-data-cmn.diff")

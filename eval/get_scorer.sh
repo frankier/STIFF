ffc0dd6ac6c29099711cab07227965bce1431935
Frankie Robertson <frankie@robertson.name>
2018-07-28 11:43:09 +0300

    Remove eval (Moved to: frankier/finn-wsd-eval)

--- eval/get_scorer.sh
+++
@@ -1,6 +0,0 @@
-wget http://lcl.uniroma1.it/wsdeval/data/WSD_Unified_Evaluation_Datasets.zip
-unzip WSD_Unified_Evaluation_Datasets.zip
-rm WSD_Unified_Evaluation_Datasets.zip
-mv WSD_Unified_Evaluation_Datasets/Scorer.java .
-rm -r WSD_Unified_Evaluation_Datasets
-javac Scorer.java

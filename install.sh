b2288d0e6a604a37c3240acfd4f62cde10b27864
Frankie Robertson <frankie@robertson.name>
2018-07-28 10:46:58 +0300

    Add fetch_omw_wikt and fetch_opensubtitles2018 to install script

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ pipenv --site-packages
 pipenv install
 pipenv run python3 -c "from nltk import download as d; d('wordnet'); d('omw'); d('punkt')"
 pipenv run python3 scripts/patch_cwn.py
+pipenv run python3 scripts/fetch_omw_wikt.py
+pipenv run python3 scripts/fetch_opensubtitles2018.py

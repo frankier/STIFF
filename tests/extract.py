dc07be64bd688daf1e42e9cae23d4224aaf486a1
Frankie Robertson <frankie@robertson.name>
2018-08-13 16:51:28 -0300

    Fix test_extract_fin

--- tests/extract.py
+++ tests/extract.py
@@ -5,6 +5,8 @@ import stiff.fixes  # noqa
 def test_extract_fin():
     tagging = get_synset_set_fin("Katso, mitä olet saanut aikaan.")
     saada_aikaan_tokens = [
-        token for token in tagging.tokens if token["wnlemma"] == "saada_aikaan"
+        token
+        for token in tagging.tokens
+        if any((tag["wnlemma"] == "saada_aikaan" for tag in token["tags"]))
     ]
     assert len(saada_aikaan_tokens) >= 1

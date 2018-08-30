d1db6fbe1963270ffbfad14217d2d43ce330fa1d
Frankie Robertson <frankie@robertson.name>
2018-08-30 23:47:49 +0300

    Parameterise mk_stiff.sh

--- scripts/mk_stiff.sh
+++ scripts/mk_stiff.sh
@@ -2,5 +2,5 @@
 
 script_path=$(dirname "$0")
 
-python $script_path/tag.py cmn-fin - \
-| zstdmt -D zstd-compression-dictionary - -o stiff.raw.xml.zstd
+python $script_path/tag.py $1 - \
+| zstdmt -D zstd-compression-dictionary - -o $2

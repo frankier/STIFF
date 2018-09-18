908f02f3165a7cfa6e3a40d11a0657db2564bbd1
Frankie Robertson <frankie@robertson.name>
2018-09-18 20:06:20 +0300

    Fix install scrip

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 pipenv --site-packages
 pipenv install
 pipenv run python3 -c "from nltk import download as d; d('wordnet'); d('omw'); d('punkt')"
-pipenv run python3 scripts/patch_cwn.py
-pipenv run python3 scripts/fetch_omw_wikt.py
+pipenv run python3 -m stiff.scripts.post_install
 pipenv run python3 scripts/fetch_opensubtitles2018.py

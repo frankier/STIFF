a8b58f01baa8aa70816a45c4846fbf79912c81c9
Frankie Robertson <frankie@robertson.name>
2018-12-03 14:53:30 +0200

    Add dominance filters

--- stiff/utils/anns.py
+++ stiff/utils/anns.py
@@ -1,11 +1,15 @@
 from urllib.parse import parse_qsl
 
 
-def get_ann_pos(ann):
+def get_ann_pos_dict(ann):
     anchor_poses = ann.attrib["anchor-positions"].split()
     assert len(anchor_poses) == 1
     anchor_pos_str = anchor_poses[0]
-    anchor_pos = dict(parse_qsl(anchor_pos_str))
+    return dict(parse_qsl(anchor_pos_str))
+
+
+def get_ann_pos(ann):
+    anchor_pos = get_ann_pos_dict(ann)
     tok = int(anchor_pos["token"])
     tok_len = int(anchor_pos["token-length"]) if "token-length" in anchor_pos else 1
     return tok, tok_len

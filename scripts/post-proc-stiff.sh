1331e4fa46020f6f31d0f35b803beb6761467ded
Frankie Robertson <frankie@robertson.name>
2019-10-29 21:23:27 +0200

    Remove post-proc-stiff (part of pipeline now)

--- scripts/post-proc-stiff.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p $2
-cp $1/corpus.xml $2/corpus.xml
-cp $1/corpus.key $2/corpus.key
-poetry run scripts/munge.py senseval-rm-lemma --lemmas olla,ei $1/corpus.sup.xml $2/corpus.sup.xml $2/keys.pkl
-poetry run scripts/munge.py senseval-rm-lemma --lemmas olla,ei $1/corpus.sup.seg.xml $2/corpus.sup.seg.xml
-poetry run scripts/munge.py senseval-rm-lemma --lemmas olla,ei $1/corpus.sup.tag.xml $2/corpus.sup.tag.xml
-poetry run scripts/munge.py key-rm-lemma --two $1/corpus.sup.key $2/corpus.sup.key $2/keys.pkl
-poetry run scripts/munge.py key-rm-lemma --three $1/corpus.sup.3.key $2/corpus.sup.3.key $2/keys.pkl

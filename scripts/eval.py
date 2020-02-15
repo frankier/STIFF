5ea269c2acc3539f894ca0daa9b0d15424cf03ba
Frankie Robertson <frankie@robertson.name>
2020-02-15 21:18:38 +0200

    Add a bunch of entropy and ambiguity eval tracing/plotting stuff

--- scripts/eval.py
+++ scripts/eval.py
@@ -3,11 +3,19 @@ from lxml import etree
 import click
 from stiff.utils.anns import get_ann_pos, get_ann_pos_dict
 from stiff.utils.xml import iter_sentences, iter_sent_to_pairs, iter_sentence_id_pairs
+from stiff.data.constants import UNI_POS_WN_MAP, WN_UNI_POS_MAP
+from stiff.sup_corpus import next_key, iter_lexelts
 import pandas as pd
-from streamz import Stream
-from streamz.dataframe import DataFrame
 from os import listdir
 from os.path import join as pjoin
+from collections import Counter
+from math import log2
+from finntk.wordnet.reader import fiwn, fiwn_encnt
+from nltk.corpus import wordnet
+from matplotlib import pylab as pl
+from matplotlib.ticker import MultipleLocator
+from glob import glob
+import seaborn as sns
 
 
 @click.group("eval")
@@ -273,89 +281,444 @@ def pr_plot(opensubs18_csv, eurosense_csv=None, out=None):
         plt.show()
 
 
-@eval.command("cov")
+def sent_report(inf, report_cb, subtotal=None):
+    sents = 0
+    done = False
+    try:
+        # XXX: take into account token length for coverage
+        for sent in iter_sentences(inf):
+            yield sent
+            sents += 1
+            if subtotal is not None and sents % subtotal == 0:
+                print(f"Report at {sents} sentences:")
+                report_cb()
+        done = True
+    finally:
+        if sents:
+            if not done:
+                print(f"Terminated early after {sents} sentences.")
+            else:
+                print(f"Finished after {sents} sentences.")
+            report_cb()
+
+
+@eval.command("intrinsic")
 @click.argument("inf", type=click.File("rb"))
 @click.argument("subtotal", type=int, required=False)
-def cov(inf, subtotal=None):
+def intrinsic(inf, subtotal=None):
     """
     Produce a report of how much of a corpus in Eurosense/STIFF format is
     covered by annotations.
     """
-    source = Stream()
-    header = pd.DataFrame(
-        {"toks": [], "anns": [], "unambg_anns": [], "uniq_anns": [], "cover": []}
-    )
-    sdf = DataFrame(source, example=header)
-    sums = {}
-    for col in ["toks", "anns", "unambg_anns", "uniq_anns", "cover"]:
-        sums[col] = getattr(sdf, col).sum().stream.gather().sink_to_list()
-
-    ambg_source = Stream()
-    ambg_header = pd.DataFrame({"ambg": []})
-    ambg_sdf = DataFrame(ambg_source, example=ambg_header)
-    ambg_hist = ambg_sdf.ambg.value_counts().stream.gather().sink_to_list()
+    sums = Counter()
+    ambg_hist = Counter()
 
     def print_cov():
-        sents = len(sums["anns"])
-        print("Coverage at {} sentences:".format(sents))
         print(
             "Total annotations, unique annotations, unambiguous annotations, "
             "tokens, tokens covered, proportion of tokens covered"
         )
         print(
-            sums["anns"][-1],
-            sums["uniq_anns"][-1],
-            sums["unambg_anns"][-1],
-            sums["toks"][-1],
-            sums["cover"][-1],
-            sums["cover"][-1] / sums["toks"][-1],
+            sums["anns"],
+            sums["uniq_anns"],
+            sums["unambg_anns"],
+            sums["toks"],
+            sums["cover"],
+            sums["cover"] / sums["toks"],
         )
-        print(ambg_hist[-1])
+        print(ambg_hist)
 
-    try:
+    for sent in sent_report(inf, print_cov, subtotal):
         # XXX: take into account token length for coverage
-        for idx, sent in enumerate(iter_sentences(inf)):
-            toks = len(sent.xpath("text")[0].text.split(" "))
-            anns = sent.xpath("annotations/annotation")
-            num_anns = len(anns)
-            ann_index = {}
-            cov_map = [0] * toks
-            for ann in anns:
-                tok, tok_len = get_ann_pos(ann)
-                ann_index.setdefault(tok, []).append(ann)
-                for idx in range(tok, tok + tok_len):
-                    cov_map[idx] += 1
-            unambg_anns = 0
-            uniq_anns = 0
-            for ann_list in ann_index.values():
-                ambg = len(ann_list)
-                ambg_source.emit(pd.DataFrame({"ambg": [ambg]}))
-                if ambg == 1:
-                    unambg_anns += 1
-                uniq_anns += 1
-            source.emit(
-                pd.DataFrame(
-                    {
-                        "toks": [toks],
-                        "anns": [num_anns],
-                        "unambg_anns": [unambg_anns],
-                        "uniq_anns": [uniq_anns],
-                        "cover": [toks - cov_map.count(0)],
-                    }
-                )
-            )
-            idx1 = idx + 1
-            if subtotal is not None and idx1 % subtotal == 0:
-                print_cov()
-    finally:
-        if len(sums["anns"]):
-            print_cov()
+        toks = len(sent.xpath("text")[0].text.split(" "))
+        anns = sent.xpath("annotations/annotation")
+        num_anns = len(anns)
+        ann_index = {}
+        cov_map = [0] * toks
+        for ann in anns:
+            tok, tok_len = get_ann_pos(ann)
+            ann_index.setdefault(tok, []).append(ann)
+            for idx in range(tok, tok + tok_len):
+                cov_map[idx] += 1
+        unambg_anns = 0
+        uniq_anns = 0
+        print("ann_index", ann_index)
+        for ann_list in ann_index.values():
+            ambg = len(ann_list)
+            ambg_hist[ambg] += 1
+            if ambg == 1:
+                unambg_anns += 1
+            uniq_anns += 1
+        sums["toks"] += toks
+        sums["anns"] += num_anns
+        sums["unambg_anns"] += unambg_anns
+        sums["uniq_anns"] += uniq_anns
+        sums["cover"] += toks - cov_map.count(0)
 
 
-@eval.command("ambg")
+def entropy(dist, total_occurs):
+    lemma_entropy = 0
+    for sense, occurs in dist.items():
+        p = occurs / total_occurs
+        if p > 0:
+            lemma_entropy -= p * log2(p)
+    return lemma_entropy
+
+
+class EntropyCalc:
+    def __init__(self):
+        self.sum_entropy = 0
+        self.sum_ambg = 0
+        self.weight_entropy = 0
+        self.weight_ambg = 0
+        self.num_insts = 0
+        self.num_lemmas = 0
+
+    def add_dist(self, dist):
+        insts = sum(dist.values())
+        h = entropy(dist, insts)
+        self.sum_entropy += h
+        self.weight_entropy += insts * h
+        self.sum_ambg += len(dist)
+        self.weight_ambg += insts * len(dist)
+        self.num_lemmas += 1
+        self.num_insts += insts
+
+    def report(self):
+        print(
+            "H",
+            self.sum_entropy / self.num_lemmas,
+            "Ambg",
+            self.sum_ambg / self.num_lemmas,
+        )
+        print(
+            "wH",
+            self.weight_entropy / self.num_insts,
+            "wAmbg",
+            self.weight_ambg / self.num_insts,
+        )
+
+
+def add_senses(dist, senses):
+    for sense in senses:
+        dist[sense] += 1 / len(senses)
+
+
+def build_uni_sense_dist(sents, keyin, vocab):
+    for sent in sents:
+        instances = sent.xpath("instance")
+        for inst in instances:
+            inst_id, senses = next_key(keyin)
+            assert inst_id == inst.attrib["id"].encode()
+            add_senses(vocab.setdefault(inst.attrib["lemma"], Counter()), senses)
+
+
+def iter_dists_sup(inf, keyin):
+    for lexelt, lemma_pos in iter_lexelts(inf):
+        dist = Counter()
+        for inst in lexelt.xpath("instance"):
+            inst_id, senses = next_key(keyin)
+            assert inst_id == inst.attrib["id"].encode()
+            add_senses(dist, senses)
+        yield dist
+
+
+@eval.command("entropy-uni")
 @click.argument("inf", type=click.File("rb"))
-def ambg(inf):
-    pass
+@click.argument("keyin", type=click.File("rb"))
+@click.argument("subtotal", type=int, required=False)
+def entropy_uni(inf, keyin, subtotal=None):
+    """
+    Works on .xml files structured around <sentence>
+    """
+    vocab = {}
+
+    def print_entropy():
+        calc = EntropyCalc()
+        for lemma, dist in vocab.items():
+            calc.add_dist(dist)
+        calc.report()
+
+    build_uni_sense_dist(sent_report(inf, print_entropy, subtotal), keyin, vocab)
+
+
+@eval.command("entropy-sup")
+@click.argument("inf", type=click.File("rb"))
+@click.argument("keyin", type=click.File("rb"))
+def entropy_sup(inf, keyin):
+    """
+    Works on .sup.xml files structured around <lexelt>
+    """
+    calc = EntropyCalc()
+    for dist in iter_dists_sup(inf, keyin):
+        calc.add_dist(dist)
+        calc.report()
+
+
+class LexAmbgCalc:
+    def __init__(self, wn):
+        self.wn = wn
+        self.sum_ambg = 0
+        self.weight_ambg = 0
+        self.num_lemmas = 0
+        self.num_insts = 0
+
+    def add_lemma(self, lemma_pos, cnt):
+        ambg = len(self.wn.lemmas(*lemma_pos))
+        assert ambg > 0
+        self.sum_ambg += ambg
+        self.weight_ambg += ambg * cnt
+        self.num_lemmas += 1
+        self.num_insts += cnt
+
+    def report(self):
+        print(
+            "lA",
+            self.sum_ambg / self.num_lemmas,
+            "wlA",
+            self.weight_ambg / self.num_insts,
+        )
+
+
+def print_ambg(lemma_pos_counts, wn):
+    calc = LexAmbgCalc(wn)
+    for lemma_pos, cnt in lemma_pos_counts.items():
+        calc.add_lemma(lemma_pos, cnt)
+    calc.report()
+
+
+def get_wn(lang):
+    if lang == "eng":
+        return wordnet
+    else:
+        return fiwn
+
+
+@eval.command("lex-ambg-uni")
+@click.argument("inf", type=click.File("rb"))
+@click.argument("lang", type=click.Choice(("eng", "fin")))
+@click.argument("subtotal", type=int, required=False)
+def lex_ambg_uni(inf, lang, subtotal=None):
+    """
+    Works on .xml files structured around <sentence>
+    """
+    lemma_counts = Counter()
+
+    for sent in sent_report(
+        inf, lambda: print_ambg(lemma_counts, get_wn(lang)), subtotal
+    ):
+        instances = sent.xpath("instance")
+        for inst in instances:
+            lemma_counts[
+                (inst.attrib["lemma"], UNI_POS_WN_MAP[inst.attrib["pos"]])
+            ] += 1
+
+
+@eval.command("lex-ambg-sup")
+@click.argument("inf", type=click.File("rb"))
+@click.argument("lang", type=click.Choice(("eng", "fin")))
+def lex_ambg_sup(inf, lang):
+    """
+    Works on .sup.xml files structured around <lexelt>
+    """
+    calc = LexAmbgCalc(get_wn(lang))
+    for lexelt, lemma_pos in iter_lexelts(inf):
+        calc.add_lemma(lemma_pos, len(lexelt.xpath("instance")))
+        calc.report()
+
+
+@eval.command("lkb-entropy-ambg")
+def lkb_entropy_ambg():
+    wns = (fiwn_encnt, wordnet)
+    for wn in wns:
+        ambg_calc = LexAmbgCalc(wn)
+        ent_calc = EntropyCalc()
+        for lemma_pos in lemma_poses(wn):
+            ambg_calc.add_lemma(lemma_pos, 1)
+            dist = {}
+            has_any = False
+            for lemma in wn.lemmas(*lemma_pos):
+                cnt = lemma.count()
+                if cnt > 0:
+                    has_any = True
+                dist[lemma.key()] = cnt
+            if not has_any:
+                continue
+            ent_calc.add_dist(dist)
+        ambg_calc.report()
+        ent_calc.report()
+
+
+def lemma_poses(wn):
+    for pos in WN_UNI_POS_MAP.keys():
+        for lemma_name in wn.all_lemma_names(pos):
+            yield lemma_name, pos
+
+
+def fix_border(ax):
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+
+@eval.command("plot-lkb-ambgs")
+@click.argument("outf", type=click.Path(), required=False)
+def plot_lkb_ambgs(outf):
+    sides = [(fiwn, Counter()), (wordnet, Counter())]
+    xs = []
+    weights = []
+    labels = ["FiWN", "PWN"]
+    bins = 0
+    for wn, hist in sides:
+        for lemma_name, pos in lemma_poses(wn):
+            hist[len(wn.lemmas(lemma_name, pos))] += 1
+        ambgs = list(hist.keys())
+        xs.append(ambgs)
+        weights.append(list(hist.values()))
+        bins = max(bins, max(ambgs))
+    fig = pl.gcf()
+    ax = pl.gca()
+    fig.set_size_inches(645.0 / 72, 441.0 / 72)
+    ax.hist(x=xs, weights=weights, label=labels, bins=bins, log=True)
+    ax.set(xlabel="Ambiguity", ylabel="Lemmas")
+    ax.set_xlim(1, 60)
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+
+    ax.legend(loc="upper right")
+    ax.set_xticks(list(range(1, 11)) + list(range(15, 60, 5)))
+    fix_border(ax)
+    # handles, labels = ax.get_legend_handles_labels()
+    # fig.legend(handles, labels, loc='upper center')
+
+    if outf:
+        pl.savefig(outf, bbox_inches="tight")
+    else:
+        pl.show()
+
+
+def lex_ambg_hist_uni(inf, wn):
+    hist = Counter()
+    for sent in iter_sentences(inf):
+        instances = sent.xpath("instance")
+        for inst in instances:
+            ambg = len(
+                wn.lemmas(inst.attrib["lemma"], UNI_POS_WN_MAP[inst.attrib["pos"]])
+            )
+            hist[ambg] += 1
+    return hist
+
+
+ENG_WSD_CORPORA = [
+    "senseval2",
+    "senseval3",
+    "semeval2007",
+    "semeval2013",
+    "semeval2015",
+]
+
+
+@eval.command("plot-test-ambgs")
+@click.argument("eurosensetestxml", type=click.File("rb"))
+@click.argument("stifftestxml", type=click.File("rb"))
+@click.argument("engwsdevaldir", type=click.Path())
+@click.argument("outf", type=click.Path(), required=False)
+def plot_test_ambgs(eurosensetestxml, stifftestxml, engwsdevaldir, outf):
+    def mk_kwargs():
+        return dict(x=[], weights=[], label=[], histtype="barstacked")
+
+    def add_kwargs(kwargs, inf, wn, label):
+        lcs = lex_ambg_hist_uni(inf, wn)
+        kwargs["x"].append(list(lcs.keys()))
+        kwargs["bins"] = max(max(lcs.keys()), kwargs.get("bins", 0))
+        kwargs["weights"].append(list(lcs.values()))
+        kwargs["label"].append(label)
+
+    fi_kwargs = mk_kwargs()
+    add_kwargs(fi_kwargs, eurosensetestxml, fiwn, "EuroSense")
+    add_kwargs(fi_kwargs, stifftestxml, fiwn, "STIFF")
+    en_kwargs = mk_kwargs()
+    for corpus in ENG_WSD_CORPORA:
+        path = list(glob(pjoin(engwsdevaldir, corpus, "*.xml")))[0]
+        with open(path, "rb") as inf:
+            add_kwargs(en_kwargs, inf, wordnet, corpus)
+    fig, (ax1, ax2) = pl.subplots(2, sharex=True, gridspec_kw={"hspace": 0.05})
+
+    fix_border(ax1)
+    fix_border(ax2)
+    fig.set_size_inches(645.0 / 72, 441.0 / 72)
+    ax1.hist(**fi_kwargs)
+    ax2.hist(**en_kwargs)
+    ax1.set_xlim(1, 49)
+    # ax1.label_outer()
+    # ax2.label_outer()
+    ax1.legend(loc="upper right")
+    ax2.legend(loc="upper right")
+    ax2.set_xlabel("Ambiguity")
+    ax1.set_ylabel("Finnish instances")
+    ax2.set_ylabel("English instances")
+    ax2.set_xticks(list(range(1, 11)) + list(range(15, 50, 5)))
+    ax2.xaxis.set_minor_locator(MultipleLocator(1))
+
+    if outf:
+        pl.savefig(outf, bbox_inches="tight")
+    else:
+        pl.show()
+
+
+@eval.command("plot-train-entropies")
+@click.argument("eurosensetrainxml", type=click.File("rb"))
+@click.argument("eurosensetrainkey", type=click.File("rb"))
+@click.argument("stifftrainxml", type=click.File("rb"))
+@click.argument("stifftrainkey", type=click.File("rb"))
+@click.argument("semcorxml", type=click.File("rb"))
+@click.argument("semcorkey", type=click.File("rb"))
+@click.argument("outf", type=click.Path(), required=False)
+def plot_train_entropies(
+    eurosensetrainxml,
+    eurosensetrainkey,
+    stifftrainxml,
+    stifftrainkey,
+    semcorxml,
+    semcorkey,
+    outf,
+):
+    fig, (ax1, ax2, ax3) = pl.subplots(3, sharex=True, gridspec_kw={"hspace": 0.05})
+
+    def add_to_data(data, dists):
+        for dist in dists:
+            insts = sum(dist.values())
+            h = entropy(dist, insts)
+            data.extend((h for _ in range(int(insts + 0.5))))
+
+    # EuroSense/STIFF
+    for inf, keyin, ax in [
+        (eurosensetrainxml, eurosensetrainkey, ax1),
+        (stifftrainxml, stifftrainkey, ax2),
+    ]:
+        data = []
+        add_to_data(data, iter_dists_sup(inf, keyin))
+        sns.distplot(data, hist_kws=dict(align="left"), ax=ax)
+    # SemCor
+    semcor_vocab = {}
+    build_uni_sense_dist(iter_sentences(semcorxml), semcorkey, semcor_vocab)
+    data = []
+    add_to_data(data, semcor_vocab.values())
+    # Plot
+    sns.distplot(data, hist_kws=dict(align="left"), ax=ax3)
+    ax3.set_xlim(-0.1)
+    ax3.set_xlabel("Entropy")
+    ax1.set_ylabel("EuroSense instance density")
+    ax2.set_ylabel("STIFF instance density")
+    ax3.set_ylabel("SemCor instance density")
+    fix_border(ax1)
+    fix_border(ax2)
+    fix_border(ax3)
+    ax3.xaxis.set_minor_locator(MultipleLocator(0.1))
+    fig.set_size_inches(441.0 / 72, 645.0 / 72)
+
+    if outf:
+        pl.savefig(outf, bbox_inches="tight")
+    else:
+        pl.show()
 
 
 if __name__ == "__main__":
